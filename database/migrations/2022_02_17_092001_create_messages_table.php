7f1bdd00d429b2cb77779d26b87aa5478f590954
Kevin <kevincowleys@gmail.com>
2022-02-17 11:33:29 +0200

    Added Admin and Messages migration

---
+++ database/migrations/2022_02_17_092001_create_messages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('comment');
+            $table->bigInteger('user_id')->nullable()->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};
