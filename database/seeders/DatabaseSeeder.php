98c6ee0467f37d0b89644bd5eae6433a51f6d083
Kevin <kevincowleys@gmail.com>
2022-02-17 11:36:23 +0200

    Added admin account seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +16,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'admin',
+            'password' => app('hash')->make('admin'),
+            'email' => 'admin@fake.com',
+            'is_admin' => true,
+        ]);
     }
 }
